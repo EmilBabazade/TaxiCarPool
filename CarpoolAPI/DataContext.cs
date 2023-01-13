b1db729c94694f1b10e0e479c438e291d8ab264d
emilbabazade20@gmail.com <emilbabazade29@gmail.com>
2023-01-13 18:18:33 +0200

    Add project files.

---
+++ CarpoolAPI/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using CarpoolAPI.Features.User;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarpoolAPI;
+
+public class DataContext : DbContext
+{
+    protected readonly IConfiguration Configuration;
+
+    public DataContext(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        // connect to sqlite database
+        options.UseSqlite(Configuration.GetConnectionString("WebApiDatabase"));
+    }
+
+    public DbSet<User> Users { get; set; }
+}
---
+++ CarpoolAPI/Features/User/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarpoolAPI.Features.User;
+
+public class User
+{
+    [Key]
+    public int Id { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public bool IsEmailConfirmed { get; set; }
+    public string PasswordHash { get; set; } = string.Empty;
+}
